49a1dff5662daeaa341571af37d3908e042ba8a6
2mer <kenyweb7@gmail.com>
2023-07-12 23:48:33 +0300

    fix: rest props on route

--- src/components/Route.tsx
+++ src/components/Route.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import { PropsWithChildren } from 'react';
-import { RequestHandler, Router } from 'express';
+import { RequestHandler, Router, RouterOptions } from 'express';
 import TargetContext, { useTarget } from '../context/TargetContext';
 
-export type RouteProps = PropsWithChildren<{
-	path?: string;
-	handlers?: RequestHandler[];
-	handler?: RequestHandler;
-}>;
+export type RouteProps = PropsWithChildren<
+	{
+		path?: string;
+		handlers?: RequestHandler[];
+		handler?: RequestHandler;
+	} & Partial<RouterOptions>
+>;
 
 function asArray<T>(v: T | T[]) {
 	if (Array.isArray(v)) return v;
 	return [v];
 }
 
-export function Route({ path = '', handler, handlers, ...rest }: RouteProps) {
+export function Route({
+	path = '',
+	handler,
+	handlers,
+	children,
+	...rest
+}: RouteProps) {
 	const target = useTarget();
-	const router = Router({ mergeParams: true });
+	const router = Router({ mergeParams: true, ...rest });
 
 	const h = handler || handlers || [];
 
 	target.use(path, [...asArray(h), router]);
 
-	return <TargetContext.Provider value={{ target: router }} {...rest} />;
+	return (
+		<TargetContext.Provider value={{ target: router }}>
+			{children}
+		</TargetContext.Provider>
+	);
 }
